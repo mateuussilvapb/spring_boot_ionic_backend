36443df09c56bae98fddb4386a4606c8302288f4
Mateus Dias <mateuussilvapb@gmail.com>
2022-03-27 19:10:11 -0300

    Testando primeira classe de dominio

--- src/main/java/com/mateuussilvapb/resources/CategoriaResource.java
+++ src/main/java/com/mateuussilvapb/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.mateuussilvapb.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mateuussilvapb.domain.Categoria;
 
 /**
  * @author Mateus
@@ -13,7 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Funcionando";
+	public List<Categoria> listar() {
+		// -----------------------------------------
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		// -----------------------------------------
+		List<Categoria> lista = new ArrayList<>();
+		// -----------------------------------------
+		lista.add(cat1);
+		lista.add(cat2);
+		// -----------------------------------------
+		return lista;
 	}
 }
