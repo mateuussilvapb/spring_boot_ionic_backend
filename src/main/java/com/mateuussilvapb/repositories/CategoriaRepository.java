ca8b353c1a2d6217fbac8666bed53da29eb0ccb0
Mateus Dias <mateuussilvapb@gmail.com>
2022-03-28 08:45:02 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/mateuussilvapb/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.mateuussilvapb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mateuussilvapb.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
--- src/main/java/com/mateuussilvapb/resources/CategoriaResource.java
+++ src/main/java/com/mateuussilvapb/resources/CategoriaResource.java
@@ -1,13 +1,14 @@
 package com.mateuussilvapb.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mateuussilvapb.domain.Categoria;
+import com.mateuussilvapb.services.CategoriaService;
 
 /**
  * @author Mateus
@@ -16,18 +17,16 @@ import com.mateuussilvapb.domain.Categoria;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	// -----------------------------------------
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	// ---------------------------------------------------------
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		// -----------------------------------------
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria obj = service.buscar(id);
 		// -----------------------------------------
-		List<Categoria> lista = new ArrayList<>();
-		// -----------------------------------------
-		lista.add(cat1);
-		lista.add(cat2);
-		// -----------------------------------------
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 }
