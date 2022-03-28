d5b6e77c131b492bb24cd1eb33e7e9e2c28d8c09
Mateus Dias <mateuussilvapb@gmail.com>
2022-03-28 15:49:46 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/mateuussilvapb/CursomcApplication.java
+++ src/main/java/com/mateuussilvapb/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mateuussilvapb.domain.Categoria;
 import com.mateuussilvapb.domain.Cidade;
+import com.mateuussilvapb.domain.Cliente;
+import com.mateuussilvapb.domain.Endereco;
 import com.mateuussilvapb.domain.Estado;
 import com.mateuussilvapb.domain.Produto;
+import com.mateuussilvapb.domain.enums.TipoCliente;
 import com.mateuussilvapb.repositories.CategoriaRepository;
 import com.mateuussilvapb.repositories.CidadeRepository;
+import com.mateuussilvapb.repositories.ClienteRepository;
+import com.mateuussilvapb.repositories.EnderecoRepository;
 import com.mateuussilvapb.repositories.EstadoRepository;
 import com.mateuussilvapb.repositories.ProdutoRepository;
 
@@ -28,6 +33,10 @@ public class CursomcApplication implements CommandLineRunner {
 	CidadeRepository cidadeRepository;
 	@Autowired
 	EstadoRepository estadoRepository;
+	@Autowired
+	ClienteRepository clienteRepository;
+	@Autowired
+	EnderecoRepository enderecoRepository;
 
 	// -----------------------------------------
 	public static void main(String[] args) {
@@ -52,6 +61,11 @@ public class CursomcApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		// -------------------------------------------------------------
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		// -------------------------------------------------------------
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		// -------------------------------------------------------------
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		// -------------------------------------------------------------
@@ -62,10 +76,15 @@ public class CursomcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		// -------------------------------------------------------------
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		// -------------------------------------------------------------
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
 	}
 
---
+++ src/main/java/com/mateuussilvapb/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.mateuussilvapb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mateuussilvapb.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
