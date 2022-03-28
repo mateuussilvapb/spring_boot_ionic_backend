2cb5b3a901f1f792ec7b1f7b93d73be7628fcba5
Mateus Dias <mateuussilvapb@gmail.com>
2022-03-28 14:59:23 -0300

    Implementacao das classes de domínio: Estado e Cidade

--- src/main/java/com/mateuussilvapb/CursomcApplication.java
+++ src/main/java/com/mateuussilvapb/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mateuussilvapb.domain.Categoria;
+import com.mateuussilvapb.domain.Cidade;
+import com.mateuussilvapb.domain.Estado;
 import com.mateuussilvapb.domain.Produto;
 import com.mateuussilvapb.repositories.CategoriaRepository;
+import com.mateuussilvapb.repositories.CidadeRepository;
+import com.mateuussilvapb.repositories.EstadoRepository;
 import com.mateuussilvapb.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,10 @@ public class CursomcApplication implements CommandLineRunner {
 	CategoriaRepository categoriaRepository;
 	@Autowired
 	ProdutoRepository produtoRepository;
+	@Autowired
+	CidadeRepository cidadeRepository;
+	@Autowired
+	EstadoRepository estadoRepository;
 
 	// -----------------------------------------
 	public static void main(String[] args) {
@@ -37,6 +45,13 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 		// -------------------------------------------------------------
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		// -------------------------------------------------------------
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		// -------------------------------------------------------------
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		// -------------------------------------------------------------
@@ -44,8 +59,14 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		// -------------------------------------------------------------
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		// -------------------------------------------------------------
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 
 }
---
+++ src/main/java/com/mateuussilvapb/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.mateuussilvapb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mateuussilvapb.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
