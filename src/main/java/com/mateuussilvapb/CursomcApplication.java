a1dc22372f3bc5ef09eec500bbbeeea95d149f26
Mateus Dias <mateuussilvapb@gmail.com>
2022-03-28 09:30:34 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/mateuussilvapb/CursomcApplication.java
+++ src/main/java/com/mateuussilvapb/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mateuussilvapb.domain.Categoria;
+import com.mateuussilvapb.domain.Produto;
 import com.mateuussilvapb.repositories.CategoriaRepository;
+import com.mateuussilvapb.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -16,6 +18,8 @@ public class CursomcApplication implements CommandLineRunner {
 	// -----------------------------------------
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	// -----------------------------------------
 	public static void main(String[] args) {
@@ -25,11 +29,23 @@ public class CursomcApplication implements CommandLineRunner {
 	// -----------------------------------------
 	@Override
 	public void run(String... args) throws Exception {
-		// -----------------------------------------
+		// -------------------------------------------------------------
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		// -----------------------------------------
+		// -------------------------------------------------------------
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		// -------------------------------------------------------------
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		// -------------------------------------------------------------
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		// -------------------------------------------------------------
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
---
+++ src/main/java/com/mateuussilvapb/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.mateuussilvapb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mateuussilvapb.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
