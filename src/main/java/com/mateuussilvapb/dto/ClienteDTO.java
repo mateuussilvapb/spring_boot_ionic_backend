21bdaf4dabd0ebd2a2c5ddf16925e7a9da5d1b63
Mateus Dias <mateuussilvapb@gmail.com>
2022-05-12 14:54:30 -0300

    Validacao customizada: email nao repetido na atualizacao de clientes

--- src/main/java/com/mateuussilvapb/dto/ClienteDTO.java
+++ src/main/java/com/mateuussilvapb/dto/ClienteDTO.java
@@ -8,7 +8,10 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.mateuussilvapb.domain.Cliente;
+import com.mateuussilvapb.services.validation.ClienteUpdate;
 
+//---------------------------------------------------------
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
