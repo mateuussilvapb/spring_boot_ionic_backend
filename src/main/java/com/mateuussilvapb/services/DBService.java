a2124685bbcb49f413d529a15773941d7d861bfc
Mateus da Silva Sousa Dias <mateuussilvapb@gmail.com>
2022-05-16 10:40:22 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/mateuussilvapb/services/DBService.java
+++ src/main/java/com/mateuussilvapb/services/DBService.java
@@ -94,7 +94,7 @@ public class DBService {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		// -------------------------------------------------------------
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "mateuussilvapb@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
 		// -------------------------------------------------------------
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
