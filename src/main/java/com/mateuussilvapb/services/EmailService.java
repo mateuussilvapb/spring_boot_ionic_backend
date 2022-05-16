720f9a3da534f20a683af0b045b81b33512b5aee
Mateus da Silva Sousa Dias <mateuussilvapb@gmail.com>
2022-05-16 16:16:34 -0300

    Email HTML

--- src/main/java/com/mateuussilvapb/services/EmailService.java
+++ src/main/java/com/mateuussilvapb/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.mateuussilvapb.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.mateuussilvapb.domain.Pedido;
@@ -12,4 +14,10 @@ public interface EmailService {
 
 	// ---------------------------------------------------------
 	void sendEmail(SimpleMailMessage msg);
+
+	// ---------------------------------------------------------
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	// ---------------------------------------------------------
+	void sendHtmlEmail(MimeMessage msg);
 }
