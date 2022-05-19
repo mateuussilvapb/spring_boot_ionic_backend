fef33314579ec00f263ad7a94433d9dda238b1a4
Mateus da Silva Sousa Dias <mateuussilvapb@gmail.com>
2022-05-19 09:10:24 -0300

    Implementando autorizacao

--- src/main/java/com/mateuussilvapb/config/SecurityConfig.java
+++ src/main/java/com/mateuussilvapb/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mateuussilvapb.security.JWTAuthenticationFilter;
+import com.mateuussilvapb.security.JWTAuthorizationFilter;
 import com.mateuussilvapb.security.JWTUtil;
 
 //-----------------------------------------
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
