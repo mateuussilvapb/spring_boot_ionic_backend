0f1fdfc3b2303be10ac4f1053c7581043201d264
Mateus da Silva Sousa Dias <mateuussilvapb@gmail.com>
2022-05-24 11:09:14 -0300

    Ajustando tamanho da imagem

--- src/main/java/com/mateuussilvapb/services/ClienteService.java
+++ src/main/java/com/mateuussilvapb/services/ClienteService.java
@@ -58,6 +58,10 @@ public class ClienteService {
 	private String prefix;
 
 	// =============================================================//
+	@Value("${img.profile.size}")
+	private Integer size;
+
+	// =============================================================//
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
 		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
@@ -142,6 +146,8 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado!");
 		}
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
