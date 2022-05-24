e9019d2d87f7f3cdba86e8b3e64ba1624970c01a
Mateus da Silva Sousa Dias <mateuussilvapb@gmail.com>
2022-05-24 10:19:05 -0300

    Salvando URL de imagem no Cliente

--- src/main/java/com/mateuussilvapb/domain/Cliente.java
+++ src/main/java/com/mateuussilvapb/domain/Cliente.java
@@ -71,6 +71,9 @@ public class Cliente implements Serializable {
 	private List<Pedido> pedidos = new ArrayList<>();
 
 	// =============================================================//
+	private String imageUrl;
+
+	// =============================================================//
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -174,6 +177,16 @@ public class Cliente implements Serializable {
 	// =============================================================//
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
+	}
+
+	// =============================================================//
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	// =============================================================//
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 
 	// =============================================================//
--- src/main/java/com/mateuussilvapb/services/ClienteService.java
+++ src/main/java/com/mateuussilvapb/services/ClienteService.java
@@ -127,7 +127,15 @@ public class ClienteService {
 
 	// =============================================================//
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		URI uri = s3Service.uploadFile(multipartFile);
+		Optional<Cliente> cli = clienteRepository.findById(user.getId());
+		cli.orElse(null).setImageUrl(uri.toString());
+		clienteRepository.save(cli.orElse(null));
+		return uri;
 	}
 
 }
